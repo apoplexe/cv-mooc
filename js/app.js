69d88ce4f77e9906d84eb6bd2a25f49f3aaee173
apoplexe <eddine.zeroual@gmail.com>
2017-04-17 03:15:16 +0200

    adding form

--- js/app.js
+++ js/app.js
@@ -1,1 +1,41 @@
-alert("test");
+var footer = document.querySelector('footer');
+var form = document.querySelector("form");
+
+var newElement = new unElement('monLien', 'a', 'ce site est incroyable', 'href', 'https://github.com/apoplexe');
+
+var addChildElement = function (el){
+  var name = document.createElement(el.type);
+
+  name.textContent = el.text;
+  name.setAttribute(el.attribute, el.attributeValue);
+  footer.appendChild(name);
+};
+
+function  unElement(name, type, text, attribute, attributeValue, parent){
+  this.name = name;
+  this.type = type;
+  this.text = text;
+  this.attribute = attribute;
+  this.attributeValue = attributeValue
+}
+
+footer.addEventListener("click", function () {
+  addChildElement(newElement);
+  addChildElement = function(){
+    console.log("Il faut regarder dans le repo cv-mooc !");
+  };
+  footer.classList.toggle("change");
+});
+
+form.addEventListener("submit", function(e){
+  console.log("bravo !");
+  e.preventDefault();
+});
+
+document.querySelector('#cool').addEventListener("input", function(e){
+  var onCool = e.target.value;
+  var helpBlock = document.querySelector('.help-block');
+
+  helpBlock.textContent = onCool;
+
+})
